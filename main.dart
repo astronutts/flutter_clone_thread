0dec17f434bac8928e7613003f766dc84e1c864a
andypsj@naver.com <andypsj@naver.com>
2023-08-31 22:45:46 +0900

    no message

--- main.dart
+++ main.dart
@@ -17,7 +17,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: "Hey! I tried to ..",
+      title: "Hey! I tried to .ddd.",
       debugShowCheckedModeBanner: false,
       home: Home(),
     );
