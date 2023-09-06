c90c0f6faceabc5619782213cf764bd3f5cc95c0
andypsj@naver.com <andypsj@naver.com>
2023-09-06 12:19:24 +0900

    * Video 기능을 넣기위해 탭을 편집

--- activity_screen.dart
+++ activity_screen.dart
@@ -2,13 +2,14 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_test_app/video/video_recording_screen.dart';
 import 'package:flutter_test_app/widgets/activity_widget.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 final tabs = [
   "All",
   "Replies",
-  "Mentions",
+  "Videos",
   "Validation",
   "Shopping",
 ];
@@ -88,33 +89,53 @@ class _DiscoverScreenState extends State<ActivityScreen> {
             ],
           ),
         ),
-        body: ListView(
-          scrollDirection: Axis.vertical,
+        body: TabBarView(
           children: [
-            ActivityWidget(
-                a_imagepath:
-                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRFN2Zj_H2NTVXQZp1YN1UrUEBmCv4nlZUSK3W6gIqeCLo5RG2Qfl7_bbwJZ11Z96QqlI&usqp=CAU",
-                a_mention: "Mentioned you!",
-                a_msg: "try anything just do it !!!!",
-                a_name: "theberryjungle"),
-            ActivityWidget(
-                a_imagepath:
-                    "https://cdn.shopify.com/s/files/1/0150/6262/products/the_sill-variant-white_gloss-zz_plant.jpg?v=1680548849",
-                a_mention: "Mentioned you!",
-                a_msg: "Here's a thread you should follow if you love youdddd",
-                a_name: "astronuts"),
-            ActivityWidget(
-                a_imagepath:
-                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkdPUrvq_PqcJ6xThm45NFBRnGYPElU28gAw&usqp=CAU",
-                a_mention: "Mentioned you!",
-                a_msg: "Here's a thread you should follow if you love youdddd",
-                a_name: "the.plantdads"),
-            ActivityWidget(
-                a_imagepath:
-                    "https://imgv3.fotor.com/images/blog-cover-image/10-profile-picture-ideas-to-make-you-stand-out.jpg",
-                a_mention: "Mentioned you!",
-                a_msg: "alway think blue and positive things",
-                a_name: "happyblue"),
+            ListView(
+              scrollDirection: Axis.vertical,
+              children: [
+                ActivityWidget(
+                    a_imagepath:
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRFN2Zj_H2NTVXQZp1YN1UrUEBmCv4nlZUSK3W6gIqeCLo5RG2Qfl7_bbwJZ11Z96QqlI&usqp=CAU",
+                    a_mention: "Mentioned you!",
+                    a_msg: "try anything just do it !!!!",
+                    a_name: "theberryjungle"),
+                ActivityWidget(
+                    a_imagepath:
+                        "https://cdn.shopify.com/s/files/1/0150/6262/products/the_sill-variant-white_gloss-zz_plant.jpg?v=1680548849",
+                    a_mention: "Mentioned you!",
+                    a_msg:
+                        "Here's a thread you should follow if you love youdddd",
+                    a_name: "astronuts"),
+                ActivityWidget(
+                    a_imagepath:
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkdPUrvq_PqcJ6xThm45NFBRnGYPElU28gAw&usqp=CAU",
+                    a_mention: "Mentioned you!",
+                    a_msg:
+                        "Here's a thread you should follow if you love youdddd",
+                    a_name: "the.plantdads"),
+                ActivityWidget(
+                    a_imagepath:
+                        "https://imgv3.fotor.com/images/blog-cover-image/10-profile-picture-ideas-to-make-you-stand-out.jpg",
+                    a_mention: "Mentioned you!",
+                    a_msg: "alway think blue and positive things",
+                    a_name: "happyblue"),
+              ],
+            ),
+            Container(
+              child: Center(child: Text('Replies')),
+            ),
+            VideoRecordingScreen(),
+            Container(
+              child: Center(
+                child: Text('Validation'),
+              ),
+            ),
+            Container(
+              child: Center(
+                child: Text('Shopping'),
+              ),
+            ),
           ],
         ),
       ),
