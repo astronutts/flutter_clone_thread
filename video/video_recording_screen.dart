0d7671a46e80774907d9ecc984ce9a55d16317b7
andypsj@naver.com <andypsj@naver.com>
2023-09-08 15:19:26 +0900

    * video_recording_screen
    - AnimationController
    1. _buttonAnimationController
    2. _progressAnimationController

--- video/video_recording_screen.dart
+++ video/video_recording_screen.dart
@@ -1,5 +1,6 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_test_app/video/video_preview_screen.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class VideoRecordingScreen extends StatefulWidget {
@@ -9,10 +10,31 @@ class VideoRecordingScreen extends StatefulWidget {
   State<VideoRecordingScreen> createState() => _VideoRecordingScreenState();
 }
 
-class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
+class _VideoRecordingScreenState extends State<VideoRecordingScreen>
+    with TickerProviderStateMixin {
   bool _hasPermission = false;
   bool _selfiemode = false;
+  String flashState = "";
   late CameraController _cameraController;
+  late FlashMode _flashMode;
+  late final AnimationController _buttonAnimationController =
+      AnimationController(
+    vsync: this,
+    duration: Duration(
+      milliseconds: 200,
+    ),
+  );
+
+  late final Animation<double> _buttonAnimation =
+      Tween(begin: 1.0, end: 1.3).animate(_buttonAnimationController);
+
+  late final AnimationController _progressAnimationControlloer =
+      AnimationController(
+    vsync: this,
+    duration: Duration(seconds: 10),
+    lowerBound: 0.0,
+    upperBound: 1.0,
+  );
 
   //카메라 초기화
   Future<void> initCamera() async {
@@ -23,6 +45,7 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
         cameras[_selfiemode ? 1 : 0], ResolutionPreset.ultraHigh);
 
     await _cameraController.initialize();
+    _flashMode = _cameraController.value.flashMode;
   }
 
   Future<void> initPermission() async {
@@ -42,17 +65,66 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
     }
   }
 
+  Future<void> _setFlashMode(FlashMode newFlashMode) async {
+    await _cameraController.setFlashMode(newFlashMode);
+    _flashMode = newFlashMode;
+    var flashState = _flashMode.toString();
+    print(flashState);
+    setState(() {});
+  }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     initPermission();
+    _progressAnimationControlloer.addListener(() {
+      setState(() {});
+    });
+    _progressAnimationControlloer.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _stopRecording();
+      }
+    });
   }
 
   Future<void> toggleSelfieMode() async {
     _selfiemode = !_selfiemode;
     await initCamera();
     setState(() {});
+  }
+
+  Future<void> _startRecording(TapDownDetails _) async {
+    if (_cameraController.value.isRecordingVideo) return;
+
+    await _cameraController.startVideoRecording();
+    _buttonAnimationController.forward();
+    _progressAnimationControlloer.forward();
+  }
+
+  Future<void> _stopRecording() async {
+    if (!_cameraController.value.isRecordingVideo) return;
+
+    _buttonAnimationController.reverse();
+    _progressAnimationControlloer.reset();
+
+    final video = await _cameraController.stopVideoRecording();
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => VideoPreviewScreen(video: video),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _buttonAnimationController.dispose();
+    _progressAnimationControlloer.dispose();
+    _cameraController.dispose();
+    // TODO: implement dispose
+    super.dispose();
   }
 
   @override
@@ -67,16 +139,63 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
                     Positioned(
                         top: 50,
                         right: 20,
-                        child: GestureDetector(
-                          onTap: () {
-                            toggleSelfieMode();
-                          },
-                          child: Icon(
-                            Icons.cameraswitch_rounded,
-                            color: Colors.black,
-                            size: 50,
+                        child: Column(
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                toggleSelfieMode();
+                              },
+                              child: Icon(
+                                Icons.cameraswitch_outlined,
+                                color: Colors.black,
+                                size: 40,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            IconButton(
+                              onPressed: () => _setFlashMode(FlashMode.off),
+                              icon: Icon(
+                                Icons.flashlight_on_outlined,
+                                color: Colors.black,
+                                size: 40,
+                              ),
+                            ),
+                          ],
+                        )),
+                    Positioned(
+                      bottom: 40,
+                      child: GestureDetector(
+                        onTapDown: _startRecording,
+                        onTapUp: (details) => _stopRecording,
+                        child: ScaleTransition(
+                          scale: _buttonAnimation,
+                          child: Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              Container(
+                                width: 70,
+                                height: 70,
+                                child: CircularProgressIndicator(
+                                  color: Colors.red.shade400,
+                                  strokeWidth: 6,
+                                  value: _progressAnimationControlloer.value,
+                                ),
+                              ),
+                              Container(
+                                width: 60,
+                                height: 60,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.red.shade400,
+                                ),
+                              ),
+                            ],
                           ),
-                        ))
+                        ),
+                      ),
+                    )
                   ],
                 )
               : null
