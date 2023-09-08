32137ea7362a580e44d05210654d557a2c7c7810
andypsj@naver.com <andypsj@naver.com>
2023-09-08 15:20:09 +0900

    * video_preview_screen
    - 저장한 비디오를 미리 보여줄 화면을 구현.

---
+++ video/video_preview_screen.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoPreviewScreen extends StatefulWidget {
+  final XFile video;
+  const VideoPreviewScreen({
+    super.key,
+    required this.video,
+  });
+
+  @override
+  State<VideoPreviewScreen> createState() => _VideoPreviewScreenState();
+}
+
+class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
+  late final VideoPlayerController _videoPlayerController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Preview Video'),
+      ),
+      body: Container(),
+    );
+  }
+}
+
+mixin VideoPlayerController {}
