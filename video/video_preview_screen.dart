f4190d879c5fdb0550c267ebcb63aeb32d650b07
andypsj@naver.com <andypsj@naver.com>
2023-09-10 14:46:59 +0900

    1. _saveToGallery 패키지를 사용하여 비디오의 레코딩이 끝났을때 프리뷰 페이지에 레코딩한 비디오를 미리보기형태로 플레이한다.

--- video/video_preview_screen.dart
+++ video/video_preview_screen.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:gallery_saver/gallery_saver.dart';
 import 'package:video_player/video_player.dart';
 
 class VideoPreviewScreen extends StatefulWidget {
@@ -17,22 +19,54 @@ class VideoPreviewScreen extends StatefulWidget {
 
 class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
   late final VideoPlayerController _videoPlayerController;
+  bool _savedVideo = false;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    _initVideo();
+  }
+
+  Future<void> _saveToGallery() async {
+    if (_savedVideo) return;
+
+    await GallerySaver.saveVideo(widget.video.path, albumName: "Thread Clone!");
+
+    _savedVideo = true;
+    setState(() {});
+  }
+
+  Future<void> _initVideo() async {
+    _videoPlayerController = VideoPlayerController.file(
+      File(widget.video.path),
+    );
+
+    await _videoPlayerController.initialize();
+    await _videoPlayerController.setLooping(true);
+    await _videoPlayerController.play();
+
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: _saveToGallery,
+              icon: FaIcon(
+                _savedVideo
+                    ? (FontAwesomeIcons.check)
+                    : (FontAwesomeIcons.download),
+              ))
+        ],
         title: Text('Preview Video'),
       ),
-      body: Container(),
+      body: _videoPlayerController.value.isInitialized
+          ? VideoPlayer(_videoPlayerController)
+          : null,
     );
   }
 }
-
-mixin VideoPlayerController {}
