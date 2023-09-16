2e81b83b696c52240e39fe0b56498d934221663e
andypsj@naver.com <andypsj@naver.com>
2023-09-16 23:29:33 +0900

    - login_view_model (로그인을 할때 view와 model 그리고 repository 사이에서 데이터를 주고 받게 도와주는 매개체) > 비동기식으로 받아오므로 asyncnotifier 사용 > _repository는 ref.read(authRepo) 값으로 초기화 > login 메서드 구성, 로그인이 되면 /home으로 이동

---
+++ authentication/view_models/login_view_model.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test_app/authentication/authentication_repo.dart';
+import 'package:flutter_test_app/utils.dart';
+import 'package:go_router/go_router.dart';
+
+class LoginViewModel extends AsyncNotifier<void> {
+  late final AuthenticationRepository _repository;
+
+  @override
+  FutureOr<void> build() {
+    _repository = ref.read(authRepo);
+  }
+
+  Future<void> login(
+    String email,
+    String password,
+    BuildContext context,
+  ) async {
+    state = const AsyncValue.loading();
+    state = await AsyncValue.guard(
+      () async => await _repository.signIn(email, password),
+    );
+    if (state.hasError) {
+      showFirebaseErrorSnack(context, state.error);
+    } else {
+      context.go("/home");
+    }
+  }
+}
+
+final loginProvider = AsyncNotifierProvider<LoginViewModel, void>(
+  () => LoginViewModel(),
+);
