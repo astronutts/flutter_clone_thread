ee3b032b0dc1d64fb9d02cfb3d4ac6e36fd05651
andypsj@naver.com <andypsj@naver.com>
2023-09-16 23:17:02 +0900

    - ref를 argu로 준 routeProvider 선언 - GoRouter 작업 > 여전히 initialLocation은 /home이지만 redirect로 먼저 던저줌. repo의 authRepo.isLoggedIn 값을 watch해서 로그인상태가 아니고, subloc ("/")이 SignUp, Login이면 우선 SignUpScreen 리턴 > 모든 스크린 routeName, routeURL 부여

--- router.dart
+++ router.dart
@@ -1,42 +1,76 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test_app/authentication/authentication_repo.dart';
+import 'package:flutter_test_app/authentication/screen/sign_up_screen.dart';
 import 'package:flutter_test_app/screens/activity_screen.dart';
 import 'package:flutter_test_app/screens/home.dart';
 import 'package:flutter_test_app/screens/home_screen.dart';
+import 'package:flutter_test_app/authentication/screen/login_screen.dart';
 import 'package:flutter_test_app/screens/privacy_screen.dart';
 import 'package:flutter_test_app/screens/search_screen.dart';
 import 'package:flutter_test_app/settings/views/setting_screen.dart';
 import 'package:flutter_test_app/screens/user_profile_screen.dart';
 import 'package:go_router/go_router.dart';
 
-final router = GoRouter(
-  initialLocation: Home.routeName,
-  routes: [
-    GoRoute(
-      path: Home.routeName,
-      builder: (context, state) => const Home(),
-    ),
-    GoRoute(
-      path: HomeScreen.routeName,
-      builder: (context, state) => const HomeScreen(),
-    ),
-    GoRoute(
-      path: SearchScreen.routeName,
-      builder: (context, state) => const SearchScreen(),
-    ),
-    GoRoute(
-      path: ActivityScreen.routeName,
-      builder: (context, state) => const ActivityScreen(),
-    ),
-    GoRoute(
-      path: UserProfileScreen.routeName,
-      builder: (context, state) => const UserProfileScreen(),
-    ),
-    GoRoute(
-      path: SettingScreen.routeName,
-      builder: (context, state) => const SettingScreen(),
-    ),
-    GoRoute(
-      path: PrivacyScreen.routeName,
-      builder: (context, state) => PrivacyScreen(),
-    ),
-  ],
-);
+final routerProvider = Provider((ref) {
+  //ref.watch(authState);
+  return GoRouter(
+    initialLocation: "/home",
+    redirect: (context, state) {
+      final isLoggedIn = ref.watch(authRepo).isLoggedIn;
+      if (!isLoggedIn) {
+        if (state.subloc != SignUpScreen.routeURL &&
+            state.subloc != LoginScreen.routeURL) {
+          return SignUpScreen.routeURL;
+        }
+      }
+      return null;
+    },
+    routes: [
+      GoRoute(
+        name: SignUpScreen.routeName,
+        path: SignUpScreen.routeURL,
+        builder: (context, state) => const SignUpScreen(),
+      ),
+      GoRoute(
+        name: LoginScreen.routeName,
+        path: LoginScreen.routeURL,
+        builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+        name: Home.routeName,
+        path: Home.routeURL,
+        builder: (context, state) => const Home(),
+      ),
+      GoRoute(
+        name: HomeScreen.routeName,
+        path: HomeScreen.routeURL,
+        builder: (context, state) => const HomeScreen(),
+      ),
+      GoRoute(
+        name: SearchScreen.routeName,
+        path: SearchScreen.routeURL,
+        builder: (context, state) => const SearchScreen(),
+      ),
+      GoRoute(
+        name: ActivityScreen.routeName,
+        path: ActivityScreen.routeURL,
+        builder: (context, state) => const ActivityScreen(),
+      ),
+      GoRoute(
+        name: UserProfileScreen.routeName,
+        path: UserProfileScreen.routeURL,
+        builder: (context, state) => const UserProfileScreen(),
+      ),
+      GoRoute(
+        name: SettingScreen.routeName,
+        path: SettingScreen.routeURL,
+        builder: (context, state) => const SettingScreen(),
+      ),
+      GoRoute(
+        name: PrivacyScreen.routeName,
+        path: PrivacyScreen.routeURL,
+        builder: (context, state) => PrivacyScreen(),
+      ),
+    ],
+  );
+});
