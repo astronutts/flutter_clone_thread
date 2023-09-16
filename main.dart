da800f4fc747579025cea4565afdd107d15c3e08
andypsj@naver.com <andypsj@naver.com>
2023-09-16 23:11:03 +0900

    - Firebase 초기화 작업
    await Firebase.initializeApp(
        options: DefaultFirebaseOptions.currentPlatform,
      );
    
    - 기존에 material widget 분리했던 것 때문에 라우터 오류가 있을거 같아서 다시 붙이고 ConsumerStatefulWidget으로 변환
    - router 자체에 provider를 씌워서 config 교체

--- main.dart
+++ main.dart
@@ -1,11 +1,12 @@
 // ignore_for_file: unused_import
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test_app/firebase_options.dart';
 import 'package:flutter_test_app/screens/activity_screen.dart';
 import 'package:flutter_test_app/screens/home.dart';
 import 'package:flutter_test_app/screens/home_screen.dart';
-import 'package:flutter_test_app/screens/material.dart';
 import 'package:flutter_test_app/screens/privacy_screen.dart';
 import 'package:flutter_test_app/router.dart';
 import 'package:flutter_test_app/screens/search_screen.dart';
@@ -19,6 +20,11 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   final preferences = await SharedPreferences.getInstance();
   final repository = DarkmodeConfigRepository(preferences);
 
@@ -34,17 +40,29 @@ Future<void> main() async {
   );
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends ConsumerStatefulWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  ConsumerState<MyApp> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyAppState extends ConsumerState<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialAppWidget();
+    return MaterialApp.router(
+      routerConfig: ref.watch(routerProvider),
+      themeMode: ref.watch(darkModeConfigProvider).darkmode
+          ? ThemeMode.dark
+          : ThemeMode.light,
+      darkTheme: ThemeData(
+        textTheme: Typography.whiteMountainView,
+        scaffoldBackgroundColor: Colors.black,
+        brightness: Brightness.dark,
+      ),
+      title: "Hey! I tried to .ddd.",
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
