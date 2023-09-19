2d7d4bca980ed4327d360f33690cac46a8bdf0e4
andypsj@naver.com <andypsj@naver.com>
2023-09-19 20:01:38 +0900

    - user의 model과 view를 연결시켜주는 view_model 생성 > build로 UserProfileModel 초기화, _repository는 userRepo 읽어옴. > 로그인중이면 현재 로그인된 uid를 불러와서 그 uid와 일치하는 profile 불러왐 저장하고 fromJson 메서드로 변환
    - createProfile에서 받아온 credential의 값들로 UserProfileModel의 인수들을 저장하고 _repository에도 profile을 저장

--- users/view_models/user_view_model.dart
+++ users/view_models/user_view_model.dart
@@ -1,16 +1,49 @@
 import 'dart:async';
-
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:auth/auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test_app/authentication/authentication_repo.dart';
 import 'package:flutter_test_app/users/models/user_profile_model.dart';
+import 'package:flutter_test_app/users/repos/user_repo.dart';
 
 class UsersViewModel extends AsyncNotifier<UserProfileModel> {
+  late final UserRepository _repository;
+  late final AuthenticationRepository _authenticationRepository;
+
   @override
-  FutureOr<UserProfileModel> build() {
+  FutureOr<UserProfileModel> build() async {
+    _repository = ref.watch(userRepo);
+    _authenticationRepository = ref.watch(authRepo);
+
+    if (_authenticationRepository.isLoggedIn) {
+      final profile =
+          await _repository.findProfile(_authenticationRepository.user!.uid);
+      if (profile != null) {
+        return UserProfileModel.fromJson(profile);
+      }
+    }
     // TODO: implement build
     return UserProfileModel.empty();
   }
+
+  Future<void> createProfile(UserCredential credential) async {
+    if (credential.user == null) {
+      throw Exception("Accout not created");
+    }
+    state = AsyncValue.loading();
+    final profile = UserProfileModel(
+      uid: credential.user!.uid,
+      name: credential.user!.displayName ?? "Anon",
+      email: credential.user!.email ?? "anon@anon.com",
+      bio: "undefined",
+      link: "undefined",
+    );
+    await _repository.createProfile(profile);
+
+    state = AsyncValue.data(profile);
+  }
 }
 
-final usersProvider = AsyncNotifierProvider(
+final usersProvider = AsyncNotifierProvider<UsersViewModel, UserProfileModel>(
   () => UsersViewModel(),
 );
