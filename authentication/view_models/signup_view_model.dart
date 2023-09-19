a6974989c5147b714c496046ffad6bb921beea50
andypsj@naver.com <andypsj@naver.com>
2023-09-19 19:52:44 +0900

    - users는 usersProvider의 변화를 감지함. - userCredential은 _authRepo의 SignUp을 진행할때 email값과 password 값을 받는다. - userProvider의 createProfile에 userCredential을 넘겨줌.

--- authentication/view_models/signup_view_model.dart
+++ authentication/view_models/signup_view_model.dart
@@ -3,8 +3,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test_app/authentication/authentication_repo.dart';
-import 'package:flutter_test_app/screens/activity_screen.dart';
 import 'package:flutter_test_app/screens/home.dart';
+import 'package:flutter_test_app/users/view_models/user_view_model.dart';
 import 'package:flutter_test_app/utils.dart';
 import 'package:go_router/go_router.dart';
 
@@ -17,11 +17,16 @@ class SignUpViewModel extends AsyncNotifier<void> {
 
   Future<void> signUp(BuildContext context) async {
     state = AsyncValue.loading();
+    final users = ref.watch(usersProvider.notifier);
     final form = ref.watch(signUpForm);
-    state = await AsyncValue.guard(() async => await _authRepo.signUp(
-          form['email'],
-          form['password'],
-        ));
+    state = await AsyncValue.guard(() async {
+      final userCredential = await _authRepo.signUp(
+        form['email'],
+        form['password'],
+      );
+
+      await users.createProfile(userCredential);
+    });
     if (state.hasError) {
       showFirebaseErrorSnack(context, state.error);
     } else {
